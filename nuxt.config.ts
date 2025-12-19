eab102da50d6f70612e9a7ec5f413c1e07cb065f
Flemming Eriksson <fleeri91@gmail.com>
2025-12-19 23:35:11 +0100

    color mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['nuxt-mongoose', 'shadcn-nuxt', 'nuxt-auth-utils'],
+  modules: [
+    'nuxt-mongoose',
+    'shadcn-nuxt',
+    'nuxt-auth-utils',
+    '@nuxtjs/color-mode',
+  ],
   mongoose: {
     uri: process.env.MONGODB_URI,
     options: {},
