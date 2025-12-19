d92c8b7b1b0e983e47a4ebc9ae997bf06bf1712d
Flemming Eriksson <fleeri91@gmail.com>
2025-12-19 23:35:02 +0100

    event start

--- app/pages/event/index.vue
+++ app/pages/event/index.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import { reactive, computed } from 'vue'
-import { Event } from '@/components/event'
-import type { EventRoot } from '~~/server/types/SvenskaSpel/Event'
 
-type Outcome = '1' | 'X' | '2'
-type ConfidenceLevel = 'UNSURE' | 'NEUTRAL' | 'SAFE'
+import { Event } from '@/components/event'
+
+import type { Outcome, ConfidenceLevel } from '@/components/event'
+import type { EventRoot } from '~~/server/types/SvenskaSpel/Event'
 
 const { data, pending, error } = await useFetch<EventRoot>('/api/event')
 
 const events = computed(() => data.value?.draws?.[0]?.events ?? [])
+const eventDate = computed(
+  () => data.value?.draws?.[0]?.closeTime ?? new Date()
+)
 
 const selections = reactive<Record<number, Outcome[]>>({})
 const confidenceLevels = reactive<Record<number, ConfidenceLevel>>({})
@@ -28,7 +31,18 @@ function handleConfidenceUpdate(eventNumber: number, value: ConfidenceLevel) {
     <div v-else-if="error">Error loading events: {{ error.message }}</div>
     <div class="w-full max-w-2xl" v-else>
       <div class="mb-8 flex justify-center">
-        <h1 class="text-2xl font-bold">Stryktipset</h1>
+        <h1 class="text-2xl font-bold">
+          <span>Stryktipset </span>
+          <NuxtTime
+            :datetime="eventDate"
+            year="numeric"
+            month="long"
+            day="numeric"
+            hour="2-digit"
+            minute="2-digit"
+            locale="sv-SE"
+          />
+        </h1>
       </div>
       <Event
         v-for="event in events"
