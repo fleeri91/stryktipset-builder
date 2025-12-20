c556e770533b1200fb22af67ecf9b2ecd534ff08
Flemming Eriksson <fleeri91@gmail.com>
2025-12-20 01:16:29 +0100

    header

---
+++ app/components/header/Header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { SidebarTrigger } from '@/components/ui/sidebar'
+</script>
+
+<template>
+  <header
+    class="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)"
+  >
+    <div class="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+      <div class="mr-auto flex items-center gap-2">
+        <SidebarTrigger class="-ml-1" />
+      </div>
+    </div>
+  </header>
+</template>
