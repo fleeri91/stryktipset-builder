2fa3126c7311ea103d8be4318511353d5361d324
Flemming Eriksson <fleeri91@gmail.com>
2025-12-20 01:16:34 +0100

    sidebar

---
+++ app/components/sidebar-menu/SidebarMenu.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { Home, LogOut } from 'lucide-vue-next'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+
+// Menu items.
+const menuItems = [
+  {
+    title: 'Hem',
+    url: '#',
+    icon: Home,
+  },
+]
+const actionItems = [
+  {
+    title: 'Logga ut',
+    url: '#',
+    icon: LogOut,
+  },
+]
+</script>
+
+<template>
+  <Sidebar>
+    <SidebarContent>
+      <SidebarGroup>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            <SidebarMenuItem v-for="item in menuItems" :key="item.title">
+              <SidebarMenuButton as-child>
+                <a :href="item.url">
+                  <component :is="item.icon" />
+                  <span>{{ item.title }}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+      <SidebarGroup class="mt-auto">
+        <SidebarGroupContent>
+          <SidebarMenu>
+            <SidebarMenuItem v-for="item in actionItems" :key="item.title">
+              <SidebarMenuButton as-child>
+                <a :href="item.url">
+                  <component :is="item.icon" />
+                  <span>{{ item.title }}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </SidebarContent>
+  </Sidebar>
+</template>
