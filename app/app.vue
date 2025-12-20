1b04dfc3e6d5efa1943fbb97519a52694efec972
Flemming Eriksson <fleeri91@gmail.com>
2025-12-20 01:16:39 +0100

    layout fix

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,28 @@
+<script setup lang="ts">
+import { Header } from './components/header'
+import { SidebarMenu } from './components/sidebar-menu'
+import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar'
+</script>
+
 <!-- app.vue -->
 <template>
   <NuxtLayout>
-    <NuxtLoadingIndicator />
-    <NuxtPage />
+    <SidebarProvider
+      :style="{
+        '--sidebar-width': 'calc(var(--spacing) * 72)',
+        '--header-height': 'calc(var(--spacing) * 12)',
+      }"
+    >
+      <NuxtLoadingIndicator />
+      <SidebarMenu />
+      <SidebarInset>
+        <Header />
+        <main class="flex flex-1 flex-col">
+          <div class="@container/main flex flex-1 flex-col gap-2">
+            <NuxtPage />
+          </div>
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   </NuxtLayout>
 </template>
