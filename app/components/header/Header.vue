b9ad8f4c4fcfea52b5f5f2b5c78820e7366a4964
Flemming Eriksson <fleeri91@gmail.com>
2025-12-20 01:23:56 +0100

    fix

--- app/components/header/Header.vue
+++ app/components/header/Header.vue
@@ -6,7 +6,7 @@ import { SidebarTrigger } from '@/components/ui/sidebar'
 
 <template>
   <header
-    class="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)"
+    class="flex h-(--header-height) shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)"
   >
     <div class="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
       <div class="mr-auto flex items-center gap-2">
