1a1e9fc459168a2ec23dabc954d4543c801205d7
Flemming Eriksson <fleeri91@gmail.com>
2025-12-21 04:01:08 +0100

    wip

--- app/app.vue
+++ app/app.vue
@@ -1,26 +1,19 @@
 <script setup lang="ts">
-import { Header } from './components/header'
 import { SidebarMenu } from './components/sidebar-menu'
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar'
 
-const open = ref(false)
+const { loggedIn, user } = useUserSession()
 </script>
 
 <!-- app.vue -->
 <template>
   <NuxtLayout>
-    <SidebarProvider
-      :open="open"
-      @update:open="open = $event"
-      :style="{
-        '--sidebar-width': 'calc(var(--spacing) * 72)',
-        '--header-height': 'calc(var(--spacing) * 12)',
-      }"
-    >
+    <SidebarProvider>
       <NuxtLoadingIndicator />
-      <SidebarMenu />
+      <template v-if="loggedIn && user">
+        <SidebarMenu />
+      </template>
       <SidebarInset>
-        <Header />
         <main class="flex flex-1 flex-col">
           <div class="@container/main flex flex-1 flex-col gap-2">
             <NuxtPage />
