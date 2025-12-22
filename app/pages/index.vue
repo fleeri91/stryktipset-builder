f1f15ba7a380541ce478043e3e0252a0e60b73cd
Flemming Eriksson <fleeri91@gmail.com>
2025-12-22 17:16:53 +0100

    refactor

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,64 +1,38 @@
 <script setup lang="ts">
+import type { EventRoot } from '~~/server/types/SvenskaSpel/Event'
+import { EventType } from '~~/server/types/SvenskaSpel/EventType'
+import EventList from '~/components/event-list/EventList.vue'
+
 definePageMeta({
   middleware: 'auth',
+  layout: 'centered',
 })
 
-import { reactive, computed } from 'vue'
+const { data, pending, error } = await useAsyncData('events', async () => {
+  const [stryktipset, europatipset] = await Promise.all([
+    $fetch<EventRoot>('/api/event', {
+      query: { type: EventType.Stryktipset },
+    }),
+    $fetch<EventRoot>('/api/event', {
+      query: { type: EventType.Europatipset },
+    }),
+  ])
 
-import { Event, EventEmpty } from '@/components/event'
-
-import type { Outcome, ConfidenceLevel } from '@/components/event'
-import type { EventRoot } from '~~/server/types/SvenskaSpel/Event'
-
-const { data, pending, error } = await useFetch<EventRoot>('/api/event')
-
-const events = computed(() => data.value?.draws?.[0]?.events ?? [])
-const hasDraws = computed(() => data.value?.draws.length ?? false)
-const eventDate = computed(
-  () => data.value?.draws?.[0]?.closeTime ?? new Date()
-)
-
-const selections = reactive<Record<number, Outcome[]>>({})
-const confidenceLevels = reactive<Record<number, ConfidenceLevel>>({})
-
-function handleSelectionsUpdate(eventNumber: number, value: Outcome[]) {
-  selections[eventNumber] = value
-}
-
-function handleConfidenceUpdate(eventNumber: number, value: ConfidenceLevel) {
-  confidenceLevels[eventNumber] = value
-}
+  return {
+    stryktipset,
+    europatipset,
+  }
+})
 </script>
 
 <template>
-  <div class="flex min-h-screen items-center justify-center p-4">
-    <div v-if="pending">Loading events...</div>
-    <div v-else-if="error">Error loading events: {{ error.message }}</div>
-    <div v-if="!hasDraws">
-      <EventEmpty />
-    </div>
-    <div class="bg-card w-full max-w-2xl rounded-2xl p-8" v-else>
-      <div class="mb-8 flex justify-center">
-        <h1 class="text-2xl font-bold">
-          <span>Stryktipset </span>
-          <NuxtTime
-            :datetime="eventDate"
-            year="numeric"
-            month="long"
-            day="numeric"
-            hour="2-digit"
-            minute="2-digit"
-            locale="sv-SE"
-          />
-        </h1>
-      </div>
-      <Event
-        v-for="event in events"
-        :key="event.eventNumber"
-        :event="event"
-        @update:selections="handleSelectionsUpdate"
-        @update:confidence="handleConfidenceUpdate"
-      />
+  <div v-if="pending">Loading…</div>
+  <div v-else-if="error">Something went wrong</div>
+
+  <div v-else>
+    <div class="grid grid-cols-2 space-x-4">
+      <EventList v-if="data" :event="data.stryktipset" title="Stryktipset" />
+      <EventList v-if="data" :event="data.europatipset" title="Europatipset" />
     </div>
   </div>
 </template>
