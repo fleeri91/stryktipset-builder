e5d8776e7875f3016e8e7037a95e7fcdab5478ef
Flemming Eriksson <fleeri91@gmail.com>
2025-12-22 22:02:50 +0100

    evet get

--- server/api/event/index.get.ts
+++ server/api/event/index.get.ts
@@ -1,11 +1,22 @@
 import { EventRoot } from '~~/server/types/SvenskaSpel/Event'
+import { EventType } from '~~/server/types/SvenskaSpel/EventType'
 
-export default defineEventHandler(async (): Promise<EventRoot> => {
+export default defineEventHandler(async (event): Promise<EventRoot> => {
   const config = useRuntimeConfig()
+  const query = getQuery(event)
+
+  const type = query.type as EventType
+
+  if (!Object.values(EventType).includes(type)) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Invalid event type',
+    })
+  }
 
   try {
     const data = await $fetch<EventRoot>(
-      `${config.public.svenskaSpelBaseUrl}stryktipset/draws?accesskey=${config.svenskaSpelSecret}`
+      `${config.public.svenskaSpelBaseUrl}${type}/draws?accesskey=${config.svenskaSpelSecret}`
     )
 
     return data
