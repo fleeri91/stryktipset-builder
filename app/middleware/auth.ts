5f9f6585b48a61f52a2941b1711436187584ac3f
Flemming Eriksson <fleeri91@gmail.com>
2025-12-22 14:51:37 +0100

    login page redirect

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -4,6 +4,6 @@ export default defineNuxtRouteMiddleware(() => {
   const { loggedIn } = useUserSession()
 
   if (!loggedIn.value) {
-    return navigateTo('/register')
+    return navigateTo('/login')
   }
 })
