0274cfe104f83fbc8510c33fecb53f7d4593cf0e
Flemming Eriksson <fleeri91@gmail.com>
2025-12-28 20:50:46 +0100

    error state

---
+++ app/components/error-state/ErrorState.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { CircleX } from 'lucide-vue-next'
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '@/components/ui/empty'
+
+const props = defineProps<{
+  title: string
+  description: string
+}>()
+</script>
+
+<template>
+  <Empty>
+    <EmptyHeader>
+      <EmptyMedia variant="icon">
+        <CircleX />
+      </EmptyMedia>
+      <EmptyTitle>{{ title }}</EmptyTitle>
+      <EmptyDescription>{{ description }}</EmptyDescription>
+    </EmptyHeader>
+  </Empty>
+</template>
