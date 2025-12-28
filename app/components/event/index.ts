18ca96945f8006a0b075135ec48ec0baaa0b0198
Flemming Eriksson <fleeri91@gmail.com>
2025-12-28 16:37:52 +0100

    removed unused component

--- app/components/event/index.ts
+++ app/components/event/index.ts
@@ -1,7 +1,6 @@
 export { default as Event } from './Event.vue'
 export { default as EventRow } from './EventRow.vue'
 export { default as EventConfidence } from './EventConfidence.vue'
-export { default as EventEmpty } from './EventEmpty.vue'
 
 export type { Outcome } from './Event.vue'
 export type { ConfidenceLevel } from './Event.vue'
