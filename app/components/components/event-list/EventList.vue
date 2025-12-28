7c8cd4b1e0a20e3e7bc1624d4f10c99cb3253cc6
Flemming Eriksson <fleeri91@gmail.com>
2025-12-28 23:45:58 +0100

    refactor

---
+++ app/components/components/event-list/EventList.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type { EventRoot } from '~~/shared/types/SvenskaSpel/Event'
+import { Card } from '@/components/ui/card'
+
+defineProps<{
+  event: EventRoot
+  title: string
+}>()
+</script>
+
+<template>
+  <section>
+    <ul class="flex flex-col gap-4">
+      <li v-for="draw in event.draws" :key="draw.drawNumber">
+        <NuxtLink
+          :to="`/event/${draw.productName.toLocaleLowerCase()}/${draw.drawNumber}`"
+        >
+          <Card
+            class="hover:bg-card/80 flex h-48 w-72 flex-col items-center justify-center gap-2 p-4 transition-colors"
+          >
+            <h2 class="text-lg font-bold">
+              {{ draw.productName ?? 'Event draw' }}
+            </h2>
+            <p class="text-gray-400">
+              <NuxtTime
+                :datetime="draw.closeTime"
+                year="numeric"
+                month="long"
+                day="numeric"
+                hour="2-digit"
+                minute="2-digit"
+                locale="sv-SE"
+              />
+            </p>
+          </Card>
+        </NuxtLink>
+      </li>
+    </ul>
+  </section>
+</template>
