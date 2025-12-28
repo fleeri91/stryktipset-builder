2c81e0168bdf4292359daa60210cf7d1ab1902e1
Flemming Eriksson <fleeri91@gmail.com>
2025-12-28 20:32:57 +0100

    result page

--- app/pages/result/index.vue
+++ app/pages/result/index.vue
@@ -1,7 +1,65 @@
+<script setup lang="ts">
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import type { ResultsHistoryRoot } from '~~/shared/types/SvenskaSpel/ResultsHistory'
+import { EventType } from '~~/shared/types/SvenskaSpel/EventType'
+
+import {
+  ResultHistoryList,
+  ResultHistoryItem,
+  ResultHistoryLoading,
+} from '~/components/result-history-list'
+
+const type = ref<EventType>(EventType.Stryktipset)
+
+const now = new Date()
+const year = ref(now.getFullYear())
+const month = ref(now.getMonth() + 1)
+
+const { data, pending, error } = await useFetch<ResultsHistoryRoot>(
+  '/api/results',
+  {
+    query: {
+      type,
+      year,
+      month,
+    },
+    watch: [type, year, month],
+  }
+)
+</script>
+
 <template>
   <div class="mx-auto max-w-2xl">
-    <div class="mt-12 flex items-center justify-between">
+    <div class="mt-12 space-y-4">
       <h1 class="text-3xl font-bold">Resultat</h1>
+
+      <Tabs v-model="type">
+        <TabsList class="w-full">
+          <TabsTrigger :value="EventType.Stryktipset" class="capitalize">
+            {{ EventType.Stryktipset }}
+          </TabsTrigger>
+          <TabsTrigger :value="EventType.Europatipset" class="capitalize">
+            {{ EventType.Europatipset }}
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent :value="type">
+          <div class="mt-4">
+            <div v-if="pending">
+              <ResultHistoryLoading :count="4" />
+            </div>
+
+            <div v-else-if="error">Något gick fel</div>
+
+            <div v-else-if="data?.resultDates.length">
+              <ResultHistoryList :results="data.resultDates">
+                <template #default="{ result }">
+                  <ResultHistoryItem :result="result" />
+                </template>
+              </ResultHistoryList>
+            </div>
+          </div>
+        </TabsContent>
+      </Tabs>
     </div>
   </div>
 </template>
