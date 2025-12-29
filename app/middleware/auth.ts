144efd4ca21c6dc86ebf03184954156b4ba27d75
Flemming Eriksson <fleeri91@gmail.com>
2025-12-29 02:32:07 +0100

    auth fix

--- app/middleware/auth.ts
+++
@@ -1,9 +0,0 @@
-import { defineNuxtRouteMiddleware, useUserSession, navigateTo } from '#imports'
-
-export default defineNuxtRouteMiddleware(() => {
-  const { loggedIn } = useUserSession()
-
-  if (!loggedIn.value) {
-    return navigateTo('/login')
-  }
-})
