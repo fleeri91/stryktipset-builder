6fc36c3fca7cb161018e0750f26f4d729e12ed8a
Flemming Eriksson <fleeri91@gmail.com>
2026-01-01 21:54:23 +0100

    Hero image

---
+++ app/components/HeroImage.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+interface Props {
+  src: string
+  alt?: string
+  height?: string
+  overlay?: boolean
+  overlayOpacity?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  alt: '',
+  overlay: true,
+  overlayOpacity: 0.4,
+})
+</script>
+
+<template>
+  <figure class="relative min-h-90 w-full overflow-hidden">
+    <!-- Background image -->
+    <NuxtImg
+      :src="src"
+      :alt="alt"
+      class="absolute inset-0 h-full w-full object-cover"
+      sizes="100vw sm:50vw md:400px"
+      preload
+    />
+
+    <!-- Overlay -->
+    <div
+      v-if="overlay"
+      class="absolute inset-0 bg-black"
+      :style="{ opacity: overlayOpacity }"
+    />
+  </figure>
+</template>
