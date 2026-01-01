9388bbe801697ae2bc7ca7c9ef80dc97063ae8d0
Flemming Eriksson <fleeri91@gmail.com>
2026-01-01 15:34:50 +0100

    loading state

---
+++ app/features/team/components/TeamLoading.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  count?: number
+}>()
+</script>
+
+<template>
+  <li class="grid gap-4 md:grid-cols-2">
+    <Card v-for="i in count ?? 5" :key="i" class="flex min-h-34 w-full p-0">
+      <Skeleton class="h-full w-full" />
+    </Card>
+  </li>
+</template>
