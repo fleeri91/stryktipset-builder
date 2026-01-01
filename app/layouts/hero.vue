97023107d68d4ad8b6340b5913c6111b9eeda586
Flemming Eriksson <fleeri91@gmail.com>
2026-01-01 21:54:16 +0100

    hero layout

---
+++ app/layouts/hero.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const heroImage = computed(() => route.meta.heroImage as string)
+const heroAlt = computed(() => route.meta.heroAlt as string)
+const center = computed(() => route.meta.center as boolean)
+</script>
+
+<template>
+  <div class="w-full">
+    <header v-if="heroImage">
+      <HeroImage :src="heroImage" :alt="heroAlt || 'Hero image'" />
+    </header>
+
+    <div v-if="center" class="flex h-full items-center justify-center">
+      <slot />
+    </div>
+    <div v-else>
+      <slot />
+    </div>
+  </div>
+</template>
